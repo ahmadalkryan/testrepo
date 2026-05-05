f24bbd24a4d8147bce00ca3295e28b251e28f8f9
amen <amen@gmail.com>
2026-05-05 07:23:17 +0300

    files added to staging area

---
+++ main.js
@@ -0,0 +1,79 @@
+// // console.log("Test browser");
+
+// // // single line comment
+
+// // window.onload = () => {
+// //   document.querySelector("h1").style.color = "green";
+// // };
+
+// // // window.alert("heroo ");
+
+// // let a = "ali";
+// // let b = "mohnmad ";
+
+// // var x = 10;
+// // var y = "20";
+
+// // console.log(`${y} ${x} "" '' ${a}`);
+// // console.log(a + " " + b);
+
+// // let title = "hello";
+// // let Descreption = "Elzero";
+// // let date = "25/10";
+
+// // console.log(Math.min(10, 9, 1));
+
+// let arr = [1, 55, 5, 6, 2, 2];
+
+// arr.unshift(1);
+// console.log(arr);
+// // a < 10
+// //   ? console.log(10)
+// //   : a >= 10 && a <= 40
+// //     ? console.log("10 To 40 ")
+// //     : a > 40
+// //       ? console.log("a> 40")
+// //       : console.log("unknown");
+
+// let n1 = arr.shift();
+// console.log(`last number is ${n1}`);
+// console.log(arr.indexOf(5));
+// console.log(arr.sort());
+// for (let i = 0; i <) {
+//  if(arr[i] ==55){
+//     break;
+//  }
+// }
+
+// function sayhello(username) {
+//   console.log(`helloo ${username}`);
+//   document.write(`<h1>hello ${us}</h1>`);
+//   return usernamr;
+// }
+// let a = say hello("ahmad");
+
+// console.log(a);
+
+// let s = (a, b) => {
+//   // in reachable Code ;
+
+//   a = a || 1;
+//   return a % b;
+// };
+
+// console.log(s(4));
+// define a Function And Call Function
+// default value for param is undefine :
+// if user doesn't  write his age
+
+// rest argument
+function calc(...numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
+calc(10, 10, 101);
+
+// length of array we dont Now it :
+
+// dynamic array
